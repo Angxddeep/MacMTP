55a9aa5b9f57777d61180b18a83e97ec60ce50f6
Angaddeep Singh <angxddeep@gmail.com>
2026-04-29 01:50:27 +0530

    Intial commit

---
+++ MacMTP/MacMTP/MacMTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMTPApp.swift
+//  MacMTP
+//
+//  Created by Angaddeep Singh on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MacMTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacMTP/MacMTPTests/MacMTPTests.swift
@@ -0,0 +1,18 @@
+//
+//  MacMTPTests.swift
+//  MacMTPTests
+//
+//  Created by Angaddeep Singh on 29/04/26.
+//
+
+import Testing
+
+struct MacMTPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
